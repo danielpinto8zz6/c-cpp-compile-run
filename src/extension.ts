57ed7707bd08959ae23618ba895b8f2694445ae6
danielpinto8zz6 <danielpinto8zz6@gmail.com>
2018-09-22 05:06:11 +0100

    Rework compiling method + log

--- src/extension.ts
+++ src/extension.ts
@@ -7,25 +7,51 @@ import { VSCodeUI } from "./VSCodeUI";
 export function activate(context: vscode.ExtensionContext) {
     let CompileRunCommand = vscode.commands.registerCommand('extension.CompileRun', () => {
         let currentFile = vscode.window.activeTextEditor.document.fileName;
-        let outputFile = path.join(path.parse(currentFile).dir, path.parse(currentFile).name);
 
         if (!currentFile) {
             return;
         }
 
+        let outputFile = path.join(path.parse(currentFile).dir, path.parse(currentFile).name);
+        if (process.platform === 'win32') {
+            outputFile = outputFile + '.exe';
+        }
+
+        const spawn = require('child_process').spawn;
+        let run;
+
+        let args = ['-Wall', '-Wextra'];
+        args.push(currentFile, '-o', outputFile);
+
         switch (path.parse(currentFile).ext) {
             case '.cpp': {
-                VSCodeUI.runInTerminal(`g++ -std=c++17 -Wall -Wextra "${ currentFile }" -o "${ outputFile }" && clear && "${ outputFile }"`);
+                run = spawn('g++', args);
                 break;
             }
             case '.c': {
-                VSCodeUI.runInTerminal(`g++ -Wall -Wextra "${ currentFile }" -o "${ outputFile }" && clear && "${ outputFile }"`);
+                run = spawn('gcc', args);
                 break;
             }
             default: {
-                break;
+                return;
             }
         }
+
+        run.stderr.on('data', (data: any) => {
+            getOutputChannel().appendLine(data);
+            getOutputChannel().show(true);
+        });
+
+        run.on('close', function (data: any) {
+            if (data === 0) {
+                // Compiled successfully let's tell the user & execute
+                vscode.window.showInformationMessage("Compiled successfuly!");
+                VSCodeUI.runInTerminal(`"${outputFile}"`);
+            } else {
+                // Error compiling
+                vscode.window.showErrorMessage("Error compiling!");
+            }
+        });
     });
 
     context.subscriptions.push(CompileRunCommand);
@@ -33,6 +59,14 @@ export function activate(context: vscode.ExtensionContext) {
     context.subscriptions.push(vscode.window.onDidCloseTerminal((closedTerminal: vscode.Terminal) => {
         VSCodeUI.onDidCloseTerminal(closedTerminal);
     }));
+
+    let _channel: vscode.OutputChannel;
+    function getOutputChannel(): vscode.OutputChannel {
+        if (!_channel) {
+            _channel = vscode.window.createOutputChannel('C/C++ Compile Run');
+        }
+        return _channel;
+    }
 }
 
 export function deactivate() {

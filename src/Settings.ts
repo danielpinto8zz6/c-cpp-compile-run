6fedcc57af03cd4853c9cf5803c3fc4a319bd273
danielpinto8zz6 <danielpinto8zz6@gmail.com>
2018-11-14 15:44:42 +0000

    Trim all strings from settings

--- src/Settings.ts
+++ src/Settings.ts
@@ -16,11 +16,11 @@ export namespace Settings {
     function getSetting<T>(name: string): T | undefined {
         return workspace.getConfiguration("c-cpp-compile-run", null).get<T>(name);
     }
-    export let cCompiler = () => getSetting<string>(key.cCompiler);
-    export let cFlags = () => getSetting<string>(key.cFlags);
-    export let cppCompiler = () => getSetting<string>(key.cppCompiler);
-    export let cppFlags = () => getSetting<string>(key.cppFlags);
+    export let cCompiler = () => getSetting<string>(key.cCompiler).trim();
+    export let cFlags = () => getSetting<string>(key.cFlags).trim();
+    export let cppCompiler = () => getSetting<string>(key.cppCompiler).trim();
+    export let cppFlags = () => getSetting<string>(key.cppFlags).trim();
     export let saveBeforeCompile = () => getSetting<boolean>(key.saveBeforeCompile);
-    export let runArgs = () => getSetting<string>(key.runArgs);
+    export let runArgs = () => getSetting<string>(key.runArgs).trim();
     export let runInExternalTerminal = () => getSetting<boolean>(key.runInExternalTerminal);
 }

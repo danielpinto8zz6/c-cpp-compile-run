30f035f9881424118ed9e2170958b426d7981b9c
danielpinto8zz6 <danielpinto8zz6@gmail.com>
2024-01-31 00:14:37 +0000

    fix: 🐛 Fix slow compilation on windows with temporary workaroun
    ✅ Closes: #339

--- src/utils/common-utils.ts
+++ src/utils/common-utils.ts
@@ -9,10 +9,20 @@ export async function commandExists(command: string): Promise<boolean> {
 }
 
 export async function isProccessRunning(proccess: string): Promise<boolean> {
-    // Temporary workaround for wsl
-    if (isWsl){
+    // Temporary workaround for windows, it is impacting the performance
+    if (isWindows()) {
         return false;
     }
+
+    // Temporary workaround for wsl
+    if (isWsl) {
+        return false;
+    }
+
     const list = await find("name", proccess, true);
     return list.length > 0;
+}
+
+export function isWindows(): boolean {
+    return process.platform === "win32";
 }
